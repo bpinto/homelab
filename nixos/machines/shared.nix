e81a7829b4722bc5aa3c7bb33146b1a0aff8b4ce
Bruno Pinto <bruno@bpinto.com>
2026-03-06 14:31:15 +0000

    refactor: only explicitly define fileSystems on vm installs

--- nixos/machines/shared.nix
+++ nixos/machines/shared.nix
@@ -21,19 +21,4 @@
   services.openssh.settings.PasswordAuthentication = false;
 
   system.stateVersion = "25.11";
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = [ "fmask=0077" "dmask=0077" ];
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-label/swap"; }
-  ];
 }
