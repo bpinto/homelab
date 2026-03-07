8f3f8ebba4491b835cd244cb39d66c0b28c42cd5
Bruno Pinto <bruno@bpinto.com>
2026-03-07 11:01:19 +0000

    feat: automatic clone homelab (#83)
    * feat(nixos): install git
    
    * feat(nixos): install neovim
    
    * feat(nixos): basic packages

--- users/hass/home-manager.nix
+++ users/hass/home-manager.nix
@@ -5,4 +5,16 @@
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
   home.stateVersion = "25.11";
+
+  programs.neovim = {
+    defaultEditor = true;
+    enable = true;
+  };
+
+  home.packages = with pkgs; [
+    git
+    htop
+    ripgrep
+    tree
+  ];
 }
