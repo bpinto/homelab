41b6be44ce8d61e709d4176884cf2835e0d78b3c
Bruno Pinto <bruno@bpinto.com>
2026-03-07 11:15:56 +0000

    feat(git): configure basic aliases

--- users/hass/home-manager.nix
+++ users/hass/home-manager.nix
@@ -11,13 +11,33 @@
   # introduces backwards incompatible changes.
   home.stateVersion = "25.11";
 
+  programs.git = {
+    enable = true;
+
+    settings = {
+      alias = {
+        amend = "commit --amend -C HEAD";
+        co = "checkout";
+        ds = "diff --staged";
+        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+        pf = "push --force-with-lease";
+        pr = "pull --rebase";
+        st = "status";
+      };
+
+      user = {
+        name = "Homelab";
+        email = "homelab@bpinto.com";
+      };
+    };
+  };
+
   programs.neovim = {
     defaultEditor = true;
     enable = true;
   };
 
   home.packages = with pkgs; [
-    git
     htop
     ripgrep
     tree
