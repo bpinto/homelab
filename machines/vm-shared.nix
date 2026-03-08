875f4d8c28aa98c48caa31b5cbc8d36782e9ee22
Bruno Pinto <bruno@bpinto.com>
2026-03-08 00:14:01 +0000

    feat(DNS): setup for VMs
    I'm experiencing "Could not resolve" error messages too frequently.

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -7,6 +7,10 @@
 
 {
   networking.hostName = "nixos-vm";
+  networking.nameservers = [
+    "1.1.1.1" # Cloudflare DNS
+    "9.9.9.9" # Quad9 DNS (security-focused)
+  ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
