bfa952e55dbfe3dde54d5af963af8328e51e3906
Bruno Pinto <bruno@bpinto.com>
2026-03-09 14:59:10 +0000

    feat(nixos): setup fileSystems for bare installations

--- machines/bare-shared.nix
+++ machines/bare-shared.nix
@@ -9,4 +9,22 @@
   hardware.enableAllFirmware = true;
 
   networking.hostName = "nixos";
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
 }
